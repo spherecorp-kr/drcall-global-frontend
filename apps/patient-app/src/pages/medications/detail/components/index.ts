fee2b28812b8c9f7b6f0b6881183db0a94706d0e
Yumin Chu <ym.chu@spherecorp.kr>
2025-11-13 18:04:33 +0900

    refactor: 필요없는 index.ts 파일 삭제

--- apps/patient-app/src/pages/medications/detail/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Section } from './Section';
-export { default as DetailHeader } from './DetailHeader';
-export { default as DeliveryInfoSection } from './DeliveryInfoSection';
-export { default as PickupInfoSection } from './PickupInfoSection';
-export { default as OrderInfoSection } from './OrderInfoSection';
-export { default as ActionButtons } from './ActionButtons';
